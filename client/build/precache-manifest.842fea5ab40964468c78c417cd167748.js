2b34df97d6e7e109f23d68a8a0889fa37949f4a8
BenJensen2 <btjensen@mtu.edu>
2020-07-24 13:29:11 -0700

    To Deploy

---
+++ client/build/precache-manifest.842fea5ab40964468c78c417cd167748.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "988c943aed165ed0dcdcaca042bb3809",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6edf2b7dc9cdbed790ae",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a16a195c6256762c32be",
+    "url": "/static/css/main.7421f4c6.chunk.css"
+  },
+  {
+    "revision": "6edf2b7dc9cdbed790ae",
+    "url": "/static/js/2.40fa7567.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.40fa7567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a16a195c6256762c32be",
+    "url": "/static/js/main.ecb4ab36.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
